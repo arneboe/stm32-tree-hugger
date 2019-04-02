c809835d10377ddff3c9f016fc355ded8e103f40
Arne Böckmann <arne@rnae.de>
2019-04-02 17:45:07 +0200

    inital main!

--- User_Src/main.cpp
+++ User_Src/main.cpp
@@ -1,7 +1,30 @@
+#include "stm32f1xx_hal.h"
 
+
+extern "C" {
+void SystemClock_Config();
+}
 
 
 int main()
 {
-  return 0;
+    SystemClock_Config();
+
+    HAL_Init();
+//     MX_GPIO_Init();
+//     MX_ADC1_Init(); 
+//     MX_USART2_UART_Init();
+//     MX_UART4_Init();
+//     MX_TIM3_Init();
+//     MX_TIM4_Init();
+//     MX_SPI1_Init();
+//     MX_USB_DEVICE_Init();
+   
+    while(1)
+    {       
+  
+    }
 }
+
+
+
