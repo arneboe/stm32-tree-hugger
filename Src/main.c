ca5b2d1676f9ac1dcec5b5a3e76024c3fc3b9150
Arne Böckmann <arne@rnae.de>
2019-05-12 16:21:18 +0200

    started adc

--- Src/main.c
+++ Src/main.c
@@ -20,6 +20,7 @@
 
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
+#include "adc.h"
 #include "gpio.h"
 
 /* Private includes ----------------------------------------------------------*/
@@ -72,6 +73,7 @@ void SystemClock_Config(void)
 {
   RCC_OscInitTypeDef RCC_OscInitStruct = {0};
   RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+  RCC_PeriphCLKInitTypeDef PeriphClkInit = {0};
 
   /** Initializes the CPU, AHB and APB busses clocks 
   */
@@ -96,6 +98,12 @@ void SystemClock_Config(void)
   RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
 
   if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_ADC;
+  PeriphClkInit.AdcClockSelection = RCC_ADCPCLK2_DIV6;
+  if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK)
   {
     Error_Handler();
   }
--- User_Src/main.cpp
+++ User_Src/main.cpp
@@ -1,31 +1,91 @@
 #include "stm32f1xx_hal.h"
 #include "gpio.h"
+#include "arm_math.h"
+#include "adc.h"
 
 extern "C" {
 void SystemClock_Config();
 }
 
+const int FFT_SIZE = 256;
+float samples[FFT_SIZE*2];
+float magnitudes[FFT_SIZE];
 
-int main()
-{
+
+
+int main() {
     SystemClock_Config();
 
     HAL_Init();
-     MX_GPIO_Init();
-//     MX_ADC1_Init(); 
+    MX_GPIO_Init();
+    MX_ADC1_Init();
+
+    if(HAL_ADC_Start_IT(&hadc1) != HAL_OK)
+    {
+        printf("ERROR STARTING HAL!\n");
+        Error_Handler();
+    }
+
+    while(true){}
+
 //     MX_USART2_UART_Init();
 //     MX_UART4_Init();
 //     MX_TIM3_Init();
 //     MX_TIM4_Init();
 //     MX_SPI1_Init();
 //     MX_USB_DEVICE_Init();
-   
-    while(1)
-    {       
-        HAL_Delay(200);
-        HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_13);
-  
+
+// start timer with  SAMPLE_RATE_HZ = 9000;
+//sample FFT_SIZE values
+/*    if (samplingIsDone()) {
+        // Run FFT on sample data.
+
+        arm_cfft_radix4_instance_f32 fft_inst;
+        arm_cfft_radix4_init_f32(&fft_inst, FFT_SIZE, 0, 1);
+        arm_cfft_radix4_f32(&fft_inst, samples);
+        arm_cmplx_mag_f32(samples, magnitudes, FFT_SIZE);
+
+
+        while (1) {
+            HAL_Delay(200);
+            HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_13);
+
+        }
+        return 0;
+    }*/
+}
+
+void HAL_ADC_ConvCpltCallback(ADC_HandleTypeDef* hadc)
+{
+    const uint32_t value = HAL_ADC_GetValue(&hadc1);
+    printf("adc: %d\n", value);
+    if(HAL_ADC_Start_IT(&hadc1) != HAL_OK)
+    {
+        printf("ERROR STARTING HAL!\n");
+        Error_Handler();
     }
+
+/*    for (int i = 0; i < NUM_FADERS; ++i)
+{
+    //Faders::faders[i].update(adcValues[i]);
+    Faders::stats[i].add(adcValues[i]);
+}
+//start next reading
+if (HAL_ADC_Start_DMA(&hadc1, (uint32_t*) adcValues, NUM_FADERS) != HAL_OK)
+    Error_Handler();*/
+}
+
+void samplingCallback() {
+    // Read from the ADC and store the sample data
+   // samples[sampleCounter] = (float32_t)analogRead(AUDIO_INPUT_PIN);
+    // Complex FFT functions require a coefficient for the imaginary part of the input.
+    // Since we only have real data, set this coefficient to zero.
+    //samples[sampleCounter+1] = 0.0;
+    // Update sample buffer position and stop after the buffer is filled
+    //sampleCounter += 2;
+    //if (sampleCounter >= FFT_SIZE*2) {
+    //    samplingTimer.end();
+    //}
 }
 
 
